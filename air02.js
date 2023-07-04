2164a9cbf91735c6391dceef7f0c845d63cbb3fb
SamyC11 <chahbisamy4@gmail.com>
2023-07-04 12:06:29 +0200

    last commit

--- air02.js
+++ air02.js
@@ -1,18 +1,18 @@
 //FUNCTIONS
 function isError(input) {
-    if (input===undefined || input==="" || input.length<3) {
+    if (input===undefined || input==="") {
         return true;
     } return false;
 }
 
-function arraySlicing (terminalArray) {
+/*function arraySlicing (terminalArray) {
     const newArray = terminalArray.slice(0,terminalArray.length-1);
     return newArray;
 }
 function initSeparator (terminalArray) {
     const newArray = terminalArray.slice(terminalArray.length-1,terminalArray.length);
     return newArray;
-}
+}*/
 
 function concatenation (array, separator) {
     let newsentence = "";
@@ -24,18 +24,18 @@ function concatenation (array, separator) {
 }
 
 //ERROR HANDLING
-const commandLineInput = process.argv.slice(2);
+const arrayOfString = process.argv.slice(2, -1);
+const stringSeparator = process.argv.slice(-1);
 
-if (isError(commandLineInput)) {
+if (isError(arrayOfString) || isError(stringSeparator) || arrayOfString.length<2) {
     console.log("error");
     process.exit(1);
 }
 //PARSING
-const arrayOfStrings = arraySlicing(commandLineInput);
-const stringSeparator = initSeparator(commandLineInput);
+
 
 //RESOLUTION
-const finalString = concatenation(arrayOfStrings, stringSeparator);
+const finalString = concatenation(arrayOfString, stringSeparator);
 
 //DISPLAY
 console.log(finalString);
